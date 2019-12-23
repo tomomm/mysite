95ea600c409f58a7cbc0e9bcbcbf5c280491f993
tomomm <576672557@qq.com>
2019-12-23 17:29:31 +0800

    modify template,add tables

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf.urls import url
 from . import view
 
 urlpatterns = [
-    path('',view.index,name='index'),
+    # path('',view.index),
+    re_path(r'^$|\bindex\b/',view.index,name='index'),
+    path('tables/',view.tables,name='tables'),
     # path('index.html/',view.index),
     path('polls/',include('polls.urls')),
     path('admin/', admin.site.urls),
--- mysite/view.py
+++ mysite/view.py
@@ -1,14 +1,27 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.template import Template
+from polls.models import Choice,Question
+from django.apps import apps
+
+def get_model_field(app_name,model_name):  #获取 数据库表字段的原始名字 和 verbose_name
+    model_obj = apps.get_model(app_name,model_name)
+    field_dic={}
+    for field in model_obj._meta.fields:
+        field_dic[field.name] = field.verbose_name
+    return field_dic
 
 def index(request):
     content = {"hello","hi","fine"}
     context = {'content':content,}
     return render(request, 'index.html', context)
 
-def table(request):
-
-    return render(request,'tables.html')
+def tables(request):
+    question_list = Question.objects.all()
+    question_attr = get_model_field('polls','Question')
+    print(question_attr)
+    print(question_list)
+    context = {'question_list':question_list,'question_attr':question_attr}
+    return render(request,'tables.html',context)
 
 
